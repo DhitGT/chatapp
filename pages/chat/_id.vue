448cd31eb8dfe5b9cf4218c24957de933413b55f
DhitGT <adityadwisaputra666@gmail.com>
2024-05-01 13:51:26 +0700

    push

---
+++ pages/chat/_id.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="chat-panel grey darken-4 pa-3 d-flex flex-column ">
+      <div v-for="(item,i) in chatItem" :key="i" style="width:fit-content" :class="{'align-self-end' : item.mine}" class="grey px-4 darken-3 pa-2 ma-3 chat-bubble rounded ">
+        <p class="text-lg">{{item.message}}</p>
+      </div>
+    </div>
+    {{contactId}}
+  </div>
+</template>
+<script>
+export default {
+  async asyncData({ params }) {
+    return { contactId: params.id };
+  },
+  data(){
+    return{
+      chatItem:[
+        {
+          message : 'lorem',
+          dibaca : false,
+          mine : false
+        },
+        {
+          message : 'lorem',
+          dibaca : false,
+          mine : false
+        },
+        {
+          message : 'lorem',
+          dibaca : false,
+          mine : true
+        },
+        {
+          message : 'lorem',
+          dibaca : false,
+          mine : false
+        },
+      ]
+    }
+  }
+}
+</script>
---
+++ pages/contact.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <v-container>
+      <v-card
+        class="rounded pa-3 ma-2"
+        v-for="(contact, i) in contactLists"
+        :key="i"
+      >
+        <div @click="handleContact(contact.contactId)">
+          <p>{{ contact.name }}</p>
+        </div>
+      </v-card>
+    </v-container>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      contactLists: [
+        {
+          name: "Udin",
+          contactId: 1,
+        },
+        {
+          name: "Jamal",
+          contactId: 2,
+        },
+        {
+          name: "Malik",
+          contactId: 3,
+        },
+        {
+          name: "Ahmad",
+          contactId: 4,
+        },
+      ],
+    };
+  },
+  methods: {
+    handleContact(id) {
+      // Navigate to the '/chat' route with the contact's ID
+      this.$router.push({ path: `/chat/${id}` });
+    },
+  },
+};
+</script>
